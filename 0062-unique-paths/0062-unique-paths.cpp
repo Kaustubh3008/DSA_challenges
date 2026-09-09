35b6336bbd450cb82590aa4c0030744b9d453537
Kaustubh3008 <kaustubhkumar46074607@gmail.com>
2026-09-10 04:12:55 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,7 @@ public:
         dp[0][0] = 1;
         for(int i =0; i<m;i++){
             for(int j=0;j<n;j++){
-                if(i == 0 && j == 0) dp[0][0] = 1;
+                if(i == 0 && j == 0) dp[i][j] = 1;
                 else{
                     int up =0;
                     int left =0;
